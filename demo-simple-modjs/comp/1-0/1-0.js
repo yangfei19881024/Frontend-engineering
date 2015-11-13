36bc0a0f207d46aceb15c38ad0ca5308a6da3ba9
yangfei <yangfei@boqii.net>
2015-11-13 18:33:14 +0800

    第三方js 转换成 模块化处理

--- demo-simple-modjs/comp/1-0/1-0.js
+++ demo-simple-modjs/comp/1-0/1-0.js
@@ -1,6 +1,28 @@
-var cal = require('../cal/cal');
+
 var list = require("../list/list");
 var API = require("../API/api");
+
+var cal = require('../cal/cal');
+
+/****==========模块化需求===========****/
+/*
+cal 模块中有个 hexs 方法需要用到 md5.js 中的方法
+所以 需要对 md5.js 模块化处理［具体见md5.js 文件］
+然后 在 cal 模块 中引用 md5 模块
+*/
+
+console.log("++++++++++");
+
+console.log(cal.hexs("yyy"));
+
+console.log("++++++++++");
+
+require("../list/zepto.min");
+
+console.log("-----");
+console.log($);
+console.log("-----");
+
 API.API();
 console.log('work');
 // 1 - 0
--- demo-simple-modjs/comp/cal/cal.js
+++ demo-simple-modjs/comp/cal/cal.js
@@ -1,5 +1,7 @@
 // @require /static/mod.js
 
+var md5 = require("../libs/md5");
+
 exports.add = function (a, b) {
     return a + b;
 };
@@ -15,3 +17,7 @@ exports.mul = function (a, b) {
 exports.min = function (a, b) {
     return a - b;
 };
+
+exports.hexs = function(name){
+  return md5.hex_md5(name);
+}
